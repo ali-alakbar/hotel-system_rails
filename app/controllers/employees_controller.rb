fd143672ec0a519c932cb964593a0c908fc29427
ali-alakbar <ali2akbar5@gmail.com>
2023-02-06 20:58:21 +0300

    rename room variable into rooms and removing spaces

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,51 +1,55 @@
 
 class EmployeesController < ApplicationController
-  before_action :find_employee_id, only: %i[show destroy]   
-    
+  before_action :find_resource, only: %i[show edit destroy]
   def index
-    @employees = Employee.all
+      @employee = Employee.all
   end
 
-  
-  def show
-    
+   def show
+      @employee = Employee.find(params[:id])
+  end
+
+  def edit
+      @employee = Employee.find(params[:id])
+  end
+
+  def update
+      @employee = Employee.find(params[:id])
+      if @employee.update(employees_params)
+          redirect_to employees_path
+      else
+          render 'new'
+      end
   end
 
 
   def new
-    @employee = Employee.new
-  end
+      @employee = Employee.new
 
-    end
-
-
-    def create
-        @employee = Employee.new(employees_params)
-        if @employee.save
-            redirect_to employees_path, notice:  "Employee is saved successfully."
-        else
-            puts "==========="
-            puts @employee.errors.full_messages
-            puts "==========="
-            render 'new'
-        end
-        flash[:notice]
-
-    end
-
-
-    def destroy
-        @employee = Employee.find(params[:id])
-        @employee.destroy
-        redirect_to employees_path
-    else
-      render 'new'
-    end
   end
 
 
-    private
-    def employees_params
-        params.require(:employee).permit(:first_name, :last_name, :email, :joining_date, :birthday, :salary, :hotel_name, :hotel_id, :age)
-    end
+  def create
+      @employee = Employee.new(employees_params)
+      if @employee.save
+          redirect_to employees_path, notice:  "Employee is saved successfully."
+      else
+          render 'new'
+      end
+      flash[:notice]
+
+  end
+
+
+ def destroy
+      @employee = Employee.find(params[:id])
+      @employee.destroy
+      redirect_to employees_path
+  end
+
+
+  private
+  def employees_params
+      params.require(:employee).permit(:first_name, :last_name, :email, :joining_date, :birthday, :salary, :hotel_name, :hotel_id, :age)
+  end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,74 +1,56 @@
 class RoomsController < ApplicationController
-  
-  before_action :find_room_id, only: %i[show destroy]   
-
   def index
-    @rooms = Room.all
+      @rooms = Room.all
   end
 
   def show
-    
+      @rooms = Room.find(params[:id])
+  end
+
+  def edit
+      @rooms = Room.find(params[:id])
+  end
+
+  def update
+      @rooms = Room.find(params[:id])
+      if @rooms.update(rooms_params)
+          redirect_to rooms_path
+      else
+          render 'new'
+      end
   end
 
 
   def new
-    @room = Room.new
+      @rooms = Room.new
+
   end
 
 
-  
   def create
-    @room = Room.new(rooms_params)
-    if @room.save
-      redirect_to rooms_path
-    else
-      render 'new'
-    end
+      @rooms = Room.new(rooms_params)
+      if @rooms.save
+          redirect_to rooms_path, notice:  "Room is saved successfully."
+      else
+          puts "==========="
+          puts @rooms.errors.full_messages
+          puts "==========="
+          render 'new'
+      end
+      flash[:notice]
+
   end
 
-  
+
   def destroy
-    @room.destroy
-    redirect_to rooms_path
+      @rooms = Room.find(params[:id])
+      @rooms.destroy
+      redirect_to rooms_path
   end
+
 
   private
-
-  def find_room_id
-    @room = Room.find(params[:id])
+  def rooms_params
+      params.require(:room).permit(:floor_no, :room_no, :max_capacity, :price, :hotel_name, :hotel_id)
   end
-
-
-    def new
-        @room = Room.new
-
-    end
-
-
-    def create
-        @room = Room.new(rooms_params)
-        if @room.save
-            redirect_to rooms_path, notice:  "Room is saved successfully."
-        else
-            puts "==========="
-            puts @room.errors.full_messages
-            puts "==========="
-            render 'new'
-        end
-        flash[:notice]
-
-    end
-
-
-    def destroy
-        @room = Room.find(params[:id])
-        @room.destroy
-        redirect_to rooms_path
-    end
-
-
-    private
-    def rooms_params
-        params.require(:room).permit(:floor_no, :room_no, :max_capacity, :price, :hotel_name, :hotel_id)
-    end
 end
