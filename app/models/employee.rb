a2e97d11c45f892bea5603dc07cd205379532428
ali-alakbar <ali2akbar5@gmail.com>
2023-02-13 23:07:47 +0300

    removing validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,18 +1,3 @@
 class Employee < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-  validates :joining_date, presence: true
-  validates :salary, presence: true, numericality: { only_float: true, greater_than: 0 }
-  validate :valid_birthday
-  
-
-  private
-
-  def valid_birthday
-    if birthday.present? && (birthday < Date.new(1940,1,1) || birthday >= Date.new(2020,1,1))
-      errors.add(:birthday, "Date is not valid, please try again.")
-    end
-  end
 
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,14 +1,4 @@
 class Room < ApplicationRecord
-  validates :room_number, presence: true, uniqueness: true
-  validates :floor_number, presence: true
-  validates :price, presence: true, numericality: { only_float: true, greater_than: 3 }
-  validates :max_capacity, presence: true, numericality: { only_integer: true, smaller_than: 7 }
-  validate :validate_reserved
 
-  private
-
-  def validate_reserved
-    errors.add(:reserved, 'must be reserved') unless reserved == true
-  end
 
 end
